9025f234a95de908aec9f1641e81698c7343169d
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-28 21:52:41 -0500

    Terminado con linter

--- test/ExplorerController.test.js
+++ test/ExplorerController.test.js
@@ -2,18 +2,18 @@ const Reader = require("./../lib/utils/reader");
 const ExplorerController = require("./../lib/controllers/ExplorerController");
 
 describe("Tests for ExplorerController", () => {
-  test("1. Regresar todos los estudiantes con todos sus campos", () => {
-    const Partners = ExplorerController.getPartners();
-    expect(Partners).not.toBeUndefined();
-  });
+    test("1. Regresar todos los estudiantes con todos sus campos", () => {
+        const Partners = ExplorerController.getPartners();
+        expect(Partners).not.toBeUndefined();
+    });
 
-  test("2. Regresar los partners con certification", () => {
-    const Partners = ExplorerController.ConCertification();
-    expect(Partners).not.toBeUndefined();
-  });
+    test("2. Regresar los partners con certification", () => {
+        const Partners = ExplorerController.ConCertification();
+        expect(Partners).not.toBeUndefined();
+    });
 
-  test("3. Regresar los partners con mas de 500 creditos", () => {
-    const Partners = ExplorerController.mayor();
-    expect(Partners).not.toBeUndefined();
-  });
+    test("3. Regresar los partners con mas de 500 creditos", () => {
+        const Partners = ExplorerController.mayor();
+        expect(Partners).not.toBeUndefined();
+    });
 });
